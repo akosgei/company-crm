ca83adf61541b6b0a3482f39322b5e98a106c90e
katao <kosgeika@gmail.com>
2022-10-15 15:33:39 +0200

    adding integration tests to asser tsome edge cases

--- src/test/java/integration/com/company/crm/rules/configuration/BusinessRulesConfigurationTest.java
+++ src/test/java/integration/com/company/crm/rules/configuration/BusinessRulesConfigurationTest.java
@@ -4,11 +4,8 @@ import com.company.crm.entity.Company;
 import com.company.crm.rules.BusinessRules;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-import java.util.Set;
 
 @SpringJUnitConfig(BusinessRulesConfiguration.class)
 class BusinessRulesConfigurationTest {
@@ -18,14 +15,11 @@ class BusinessRulesConfigurationTest {
     @MockBean
     private BusinessRulesConfiguration businessRulesConfiguration;
 
-    @Autowired
-    private Set<BusinessRules<Company>> setOfBeans;
-
     /**
      * Method under test: {@link BusinessRulesConfiguration#findBusinessRule(BusinessRules.BusinessRule)}
      */
     @Test
-    void testFindBusinessRule() {
+    void testFindBusinessRuleSignUpDateRule() {
         Mockito.when(businessRulesConfiguration.findBusinessRule(Mockito.any())).thenReturn(businessRules);
         businessRulesConfiguration.findBusinessRule(BusinessRules.BusinessRule.FILTER_BY_SIGN_UP_DATE_RULE);
         Mockito.verify(businessRulesConfiguration).findBusinessRule(Mockito.any());
@@ -35,7 +29,7 @@ class BusinessRulesConfigurationTest {
      * Method under test: {@link BusinessRulesConfiguration#findBusinessRule(BusinessRules.BusinessRule)}
      */
     @Test
-    void testFindBusinessRule2() {
+    void testFindRemoveDuplicateThreadsRule() {
         Mockito.when(businessRulesConfiguration.findBusinessRule(Mockito.any())).thenReturn(businessRules);
         businessRulesConfiguration.findBusinessRule(BusinessRules.BusinessRule.REMOVE_DUPLICATE_THREADS_RULE);
         Mockito.verify(businessRulesConfiguration).findBusinessRule(Mockito.any());
